de92f5afac837c19fe0db4201e29799ec20f0386
Stella Odewumi <stellaodewumi@gmail.com>
2016-04-24 18:02:17 -0500

    DateTimePicker date now reflects date user clicks on calendar

--- app/static/main.js
+++ app/static/main.js
@@ -1,34 +1,37 @@
 var CalendarModule = (function () {
+    // Todo remove public variables in public methods. This is overkill
     var calendarObj = {};
     calendarObj.now = moment();
     calendarObj.display_date = calendarObj.now;
-
+    calendarObj.startOfMonth = undefined;
 
     calendarObj.addMonth = function () {
-        calendarObj.display_date = calendarObj.display_date.add(1, 'months')
-        calendarObj.getDaysInMonth()
+        calendarObj.display_date = calendarObj.display_date.add(1, 'months');
+        calendarObj.getDaysInMonth();
     }
 
     calendarObj.subtractMonth = function () {
-        calendarObj.display_date = calendarObj.display_date.add(-1, 'months')
+        calendarObj.display_date = calendarObj.display_date.add(-1, 'months');
+        calendarObj.getDaysInMonth();
     }
 
     calendarObj.getDaysInMonth = function () {
-        calendarObj.display_days_in_month = calendarObj.display_date.daysInMonth()
+        calendarObj.display_days_in_month = calendarObj.display_date.daysInMonth();
     }
 
     calendarObj.getstartOfMonth = function () {
-        calendarObj.startOfMonth = calendarObj.display_date.clone().startOf('month')
-        calendarObj.startOfMonth = moment(calendarObj.startOfMonth).format("d")
+        calendarObj.startOfMonth = calendarObj.display_date.clone().startOf('month');
+        calendarObj.startOfMonth = moment(calendarObj.startOfMonth).format("d");
     }
 
-    calendarObj.getendOfMonth = function () {
-        calendarObj.endOfMonth = calendarObj.display_date.clone().endOf('month')
-        calendarObj.endOfMonth = moment(calendarObj.endOfMonth).format("D")
+    calendarObj.getDayClicked = function (date) {
+        calendarObj.getstartOfMonth()
+        return calendarObj.display_date.clone().startOf('month').add(date-1, 'days')
     }
+
     calendarObj.getDaysInMonth();
     calendarObj.getstartOfMonth();
-    calendarObj.getendOfMonth();
+
     return calendarObj
 
 })();
@@ -36,40 +39,41 @@ var CalendarModule = (function () {
 function builDaysOfWeek() {
     var daysOfWeek = ["Sunday", "Monday", "Tuesday", 
         "Wednesday", "Thursday", "Friday", "Saturday"];
-    calendarDays = "<div class='row row--calendar'>"
+    calendarDays = "<div class='row row--calendar'>";
 
     for( var i=0; i < daysOfWeek.length; i++) {
 
-      calendarDays += "<div class='col-md-1'> "+daysOfWeek[i] +"</div>"
+      calendarDays += "<div class='col-md-1'> "+daysOfWeek[i] +"</div>";
     }
 
-    calendarDays += "</div>"
+    calendarDays += "</div>";
 
-    return calendarDays
+    return calendarDays;
 }
 
 function buildCalendar() {
   var day = 1;
   var total = 0;
   var calendar = "";
-  var total_time = CalendarModule.display_days_in_month + (CalendarModule.startOfMonth-1)
+  var total_time = CalendarModule.display_days_in_month + (CalendarModule.startOfMonth-1);
 
   for(var i=0; i < 6; i++) {
-    calendar += "<div class='row row--calendar'>"
+    calendar += "<div class='row row--calendar'>";
     
     for(var j=0; j<7; j++) {
         if (total >= CalendarModule.startOfMonth && total <= total_time){
-            calendar += "<div class='col-md-1'>"+ day +"</div>";
-            day++
+            calendar += "<div class='col-md-1' data-date-of-month="+day+">"+ day +"</div>";
+            day++;
         } else {
             calendar += "<div class='col-md-1'></div>";
         }
-        total++
+        total++;
     }
     calendar += "</div>";
   }
   return calendar;
 }
+
 
 $('#calendar').html(buildCalendar())
 $('#calendar-days').html(builDaysOfWeek())
@@ -98,8 +102,12 @@ $('#prevMonth').on('click', function () {
 });
 
 $('.row--calendar').on("click", 'div', function () {
-    $('#eventModal').modal('show')
+    $('#eventModal').modal('show');
+    date_of_month = CalendarModule.getDayClicked($(this).data('date-of-month'));
+    $('#day-input').data("DateTimePicker").date(moment(date_of_month,"DD/MM/YYYY"));
+
 });
+
 
 
 $(function () {
