ed24f747a133f8a5c9235c9b58ce8226fdcf1d86
Stella Odewumi <stellaodewumi@gmail.com>
2016-04-24 11:09:35 -0500

    Added bootstrap-datetimepicker to add event form

--- app/static/main.js
+++ app/static/main.js
@@ -1,3 +1,17 @@
 $('.row--calendar').on("click", 'div', function () {
-	$('#eventModal').modal('show') 
+    $('#eventModal').modal('show')
+});
+
+$(function () {
+    $('#day-input').datetimepicker({
+        format: 'MM/DD/YYYY'
+    });
+
+    $('#start-time-input').datetimepicker({
+        format: 'h:mm a'
+    });
+
+    $('#end-time-input').datetimepicker({
+        format: 'h:mm a'
+    });
 });
