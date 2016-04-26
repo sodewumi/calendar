5e559918a0bc99f56d6263ec6b1b779fa96f2ab0
Stella Odewumi <stellaodewumi@gmail.com>
2016-04-26 13:35:33 -0500

    Fully encapsulated CalendarModule in main.js. Moved all events to .ready()

--- app/static/main.js
+++ app/static/main.js
@@ -1,38 +1,41 @@
-var CalendarModule = (function () {
-    // Todo remove public variables in public methods. This is overkill
+var socket = io.connect('http://' + document.domain + ':' + location.port + '/calendar_app');
+
+var CalendarModule = (function (moment) {
+
     var calendarObj = {};
-    calendarObj.now = moment();
-    calendarObj.display_date = calendarObj.now;
-    calendarObj.startOfMonth = undefined;
+    var displayDate = moment();
 
     calendarObj.addMonth = function () {
-        calendarObj.display_date = calendarObj.display_date.add(1, 'months');
-        calendarObj.getDaysInMonth();
+        displayDate = displayDate.add(1, "months");
+        return displayDate
     }
 
     calendarObj.subtractMonth = function () {
-        calendarObj.display_date = calendarObj.display_date.add(-1, 'months');
-        calendarObj.getDaysInMonth();
+        displayDate = displayDate.add(-1, "months");
+        return displayDate
     }
 
     calendarObj.getDaysInMonth = function () {
-        calendarObj.display_days_in_month = calendarObj.display_date.daysInMonth();
+        return displayDate.daysInMonth();
     }
 
     calendarObj.getstartOfMonth = function () {
-        calendarObj.startOfMonth = calendarObj.display_date.clone().startOf('month');
-        calendarObj.startOfMonth = moment(calendarObj.startOfMonth).format("d");
+        var startOfMonth = displayDate.clone().startOf('month');
+        var startOfMonth2 = displayDate.clone()
+        startOfMonth = moment(startOfMonth).format("d");
+
+        return startOfMonth
     }
 
-    calendarObj.getDayClicked = function (date) {
-        calendarObj.getstartOfMonth()
-        return calendarObj.display_date.clone().startOf('month').add(date-1, 'days')
+    calendarObj.getDateClicked = function (date) {
+
+        return displayDate.clone().startOf('month').add(date-1, 'days')
     }
 
-    // sp build
-    calendarObj.builDaysOfWeek = function () {
+    calendarObj.buildDaysOfWeek = function () {
         var daysOfWeek = ["Sunday", "Monday", "Tuesday", 
             "Wednesday", "Thursday", "Friday", "Saturday"];
+        // TODO change to array
         calendarDays = "<div class='row row--calendar'>";
 
         for( var i=0; i < daysOfWeek.length; i++) {
@@ -45,145 +48,145 @@ var CalendarModule = (function () {
         return calendarDays;
     }
 
+    calendarObj.getCalendarTitle = function () {
+        return moment(displayDate).format("MMMM YYYY")
+    }
+
     calendarObj.buildCalendar = function () {
-      var day = 1;
-      // change var
-      var total = 0;
+      var date = 1;
+      var calendar_cell_cnt = 0;
+      // TODO change to array
       var calendar = "";
-      // change var snake
-      var total_time = CalendarModule.display_days_in_month + (CalendarModule.startOfMonth-1);
+      var daysInMonth = calendarObj.getDaysInMonth();
+      var startOfMonthIndex = calendarObj.getstartOfMonth();
+      var endOfCalendar = daysInMonth + (startOfMonthIndex-1);
 
       for(var i=0; i < 6; i++) {
         calendar += "<div class='row row--calendar'>";
         
         for(var j=0; j<7; j++) {
-            if (total >= CalendarModule.startOfMonth && total <= total_time){
-                calendar += "<div class='col-md-1 dated' data-date-of-month="+day+">"+ day +"</div>";
-                day++;
+            // move to multiline
+            if (calendar_cell_cnt>= startOfMonthIndex && calendar_cell_cnt <= endOfCalendar){
+                calendar += "<div class='col-md-1 dated' data-date-of-month="+date+">"
+                    + date +"</div>";
+                date++;
             } else {
                 calendar += "<div class='col-md-1'></div>";
             }
-            total++;
+            calendar_cell_cnt++;
         }
         calendar += "</div>";
       }
       return calendar;
     }
 
-    calendarObj.getDaysInMonth();
-    calendarObj.getstartOfMonth();
-
     return calendarObj
 
 
-})();
+})(moment);
 
 
 
 
 $(document).ready(function () {
-    function foo (change_month) {
-        change_month();
-        // CalendarModule.getDaysInMonth();
-        CalendarModule.getstartOfMonth();
-        $("#calendar").empty()
-        $('#calendar').html(CalendarModule.buildCalendar())
+    function buildDisplayCalendar () {
+        $("#calendar").empty();
+        $('#calendar').html(CalendarModule.buildCalendar());
+        $('#calendar-title').text(CalendarModule.getCalendarTitle());
+    }
 
-        $('#calendar-title').text(moment(CalendarModule.display_date).format("MMMM YYYY"));   
+    function updateMonth (change_month) {
+        change_month();
+        buildDisplayCalendar()
+    }
+
+    function updateDatetimeObjHourMin (dateClickedDatetimeObj, datetimeObj) {
+        var updateDatetimeObj = dateClickedDatetimeObj.clone().hour(
+            datetimeObj.hour()
+        ).minute(
+            datetimeObj.minute()
+        );
+
+        return updateDatetimeObj
     }
 
     $('#calendar').on('click', '.dated', function () {
         $('#eventModal').modal('show');
-        var dateOfMonth = CalendarModule.getDayClicked($(this).data('date-of-month'));
-        console.log($('#date-input').data("DateTimePicker"))
+        var dateOfMonth = CalendarModule.getDateClicked($(this).data('date-of-month'));
         $('#date-input').data("DateTimePicker").date(moment(dateOfMonth,"DD/MM/YYYY"));
+        $('#end-time-input').data("DateTimePicker")
     });
 
     $('#nextMonth').on('click', function () {
-        foo(CalendarModule.addMonth);
+        updateMonth(CalendarModule.addMonth);
     });
 
     $('#prevMonth').on('click', function () {
-        foo(CalendarModule.subtractMonth)
+        updateMonth(CalendarModule.subtractMonth)
     });
 
-    // remove main
-    function main () {
-        CalendarModule.getDaysInMonth();
-        CalendarModule.getstartOfMonth();
-        $('#calendar').html(CalendarModule.buildCalendar())
-        $('#calendar-days').html(CalendarModule.builDaysOfWeek())
-        $('#calendar-title').text(moment(CalendarModule.now).format("MMMM YYYY"));
+    $('#calendar-days').html(CalendarModule.buildDaysOfWeek())
+    buildDisplayCalendar()
 
-        $('#date-input').datetimepicker({
-            format: 'MM/DD/YYYY'
-        });
 
-        $('#start-time-input').datetimepicker({
-            format: 'h:mm a'
-        });
+    $('#date-input').datetimepicker({
+        format: 'MM/DD/YYYY'
+    });
 
-        $('#end-time-input').datetimepicker({
-            format: 'h:mm a'
-        });
-    }
+    $('#start-time-input').datetimepicker({
+        format: 'h:mm a'
+    });
 
-    main()
+    $('#end-time-input').datetimepicker({
+        format: 'h:mm a'
+    });
+
+    $("#add-event-form").on("submit", function (evt) {
+        evt.preventDefault();
+
+        var dateClickedDatetimeObj = $('#date-input').data("DateTimePicker").date();
+        var startDatetimeObj = $('#start-time-input').data("DateTimePicker").date();
+        var endDatetimeObj = $('#end-time-input').data("DateTimePicker").date();
+        var titleVal = $('#event_title').val();
+        var calendarURL = $("#calendar").data("calendar-url");
+
+        var startDatetimeObj = updateDatetimeObjHourMin(dateClickedDatetimeObj, startDatetimeObj);
+        var endDatetimeObj = updateDatetimeObjHourMin(dateClickedDatetimeObj, endDatetimeObj);
+
+        var startTimestampUTC = startDatetimeObj.utc().format();
+        var endTimestampUTC = endDatetimeObj.utc().format();
+
+        var eventObj = {data: {
+            startTimestampUTC: startTimestampUTC,
+            endTimestampUTC: endTimestampUTC,
+            eventTitle: titleVal,
+            calendarURL: calendarURL
+        }};
+
+        socket.emit('add calendar event', eventObj);
+    });
+
+    socket.on('connect', function() {
+        var calendarURL = $("#calendar").data("calendar-url")
+        // Todo move to function if used again
+        socket.emit("join room", {calendarURL: calendarURL})
+    });
+
+    socket.on('add calendar event response', function(msg) {
+        console.log(msg)
+    });
+
+    socket.on('join room response', function(msg) {
+        console.log(msg)
+    });
+
+    // socket.on('leave room status', function(msg) {
+    //     console.log(msg)
+    // });
 });
 
-// socket.on('connect', function() {
-//     var calendarURL = $("#calendar").data("calendar-url")
-//     // Todo move to function if used again
-//     socket.emit("join room", {calendarURL: calendarURL})
-// });
 
 
-$("#add-event-form").on("submit", function (evt) {
-    evt.preventDefault();
-    var titleVal = $('#event_title').val()
-    // mutator
-    var dateWithTimeZone = $('#date-input').data("DateTimePicker").date()
-    // var dateWithoutTimeZone = dateWithTimeZone.utc()
 
-    var startTimestampVal = $('#start-time-input').data("DateTimePicker").date()
-    var endTimestampVal = $('#end-time-input').data("DateTimePicker").date();
 
-    // these are objects not timestamps. Are hour and minute mutators?
-    var startTimestampTimeZone = dateWithTimeZone.clone().hour(
-            startTimestampVal.hour()
-        ).minute(
-            startTimestampVal.minute()
-        );
 
-    var endTimestampTimeZone = dateWithTimeZone.clone().hour(
-            endTimestampVal.hour()
-        ).minute(
-            endTimestampVal.minute()
-        );
-
-    var startTimestampUTC = startTimestampTimeZone.utc().format();
-    var endTimestampUTC = endTimestampTimeZone.utc().format();
-
-    var calendarURL = $("#calendar").data("calendar-url")
-
-    var foo = {data: {
-        startTimestampUTC: startTimestampUTC,
-        endTimestampUTC: endTimestampUTC,
-        eventTitle: titleVal,
-        calendarURL: calendarURL
-    }};
-
-    socket.emit('add calendar event', foo);
-})
-
-socket.on('add calendar event response', function(msg) {
-    console.log(msg)
-});
-
-socket.on('join room response', function(msg) {
-    console.log(msg)
-});
-
-// socket.on('leave room status', function(msg) {
-//     console.log(msg)
-// });
